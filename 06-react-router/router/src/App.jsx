0b762567ea1d39d864b20e19a39f72524c9e1200
Silas Silva <ssilva@vertigo.com.br>
2024-05-23 11:53:30 -0300

    Using Link in the React Router

--- 06-react-router/router/src/App.jsx
+++ 06-react-router/router/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Sobre from './Sobre';
+import Login from './Login';
 import Header from './Header';
 import NaoEncontrada from './NaoEncontrada';
 
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="sobre" element={<Sobre />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<NaoEncontrada />} />
       </Routes>
     </BrowserRouter>
