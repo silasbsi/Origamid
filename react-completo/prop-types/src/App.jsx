b623d9220fd51576ce4173e6c93cd095977a4431
Silas Silva <ssilva@vertigo.com.br>
2024-05-27 08:41:39 -0300

    Using prop-types

---
+++ react-completo/prop-types/src/App.jsx
@@ -0,0 +1,13 @@
+import Button from "./Button";
+
+const App = () => {
+   return (
+      <div>
+         <Button margin="20px" width={300}>
+            Clique aqui
+         </Button>
+      </div>
+   );
+};
+
+export default App;
